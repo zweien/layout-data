932428630ae2c23919f8826de17cf2d67dd2c9bc
zweien <278954153@qq.com>
2020-04-09 14:20:04 +0800

    add confest.py

---
+++ layout_data/tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+import math
+import numpy as np
+import os
+import scipy.io as sio
+
+@pytest.fixture(scope='session')
+def prepare_data_path(tmpdir_factory):
+    tmpdir = tmpdir_factory.mktemp("data")
+    num = 20
+    shape = (200, 200)
+    for subdir in ['train', 'test']:
+        subdir_path = os.path.join(tmpdir, subdir)
+        os.mkdir(subdir_path)
+        for i in range(num):
+            u = np.random.randn(*shape)
+            F = np.random.randn(*shape)
+
+            path = os.path.join(subdir_path, f'{i}.mat')
+            sio.savemat(path, {'u': u, 'F': F})
+
+    return tmpdir
--- layout_data/tests/test_lightning_model.py
+++ layout_data/tests/test_lightning_model.py
@@ -1,15 +1,22 @@
 import pytest
 import math
+import numpy as np
+import os
+import scipy.io as sio
+from pathlib import Path
 from argparse import ArgumentParser
 from layout_data.models.fpn.model import FPNModel
 
 
-def test_fpn_lightning():
+
+def test_fpn_lightning(prepare_data_path):
+    path = prepare_data_path
+
     parser = ArgumentParser()
     parser = FPNModel.add_model_specific_args(parser)
     hparams = parser.parse_args()
     hparams.gpus = 1
-    hparams.data_root = 'd:/work/layout-dataset'
+    hparams.data_root = path
 
     model = FPNModel(hparams)
     model.prepare_data()
@@ -17,6 +24,7 @@ def test_fpn_lightning():
     dataloader = model.train_dataloader()
 
     F, u = next(iter(dataloader))
+    assert u.shape == (hparams.batch_size, 1, 200, 200)
     u_pred = model(F)
     assert u_pred.shape == (hparams.batch_size, 1, 200, 200)
 
