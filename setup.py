9e5845937d379e14b85d5d6f774ff2cb29001b36
zweien <278954153@qq.com>
2020-04-17 22:49:53 +0800

    docs: fix md table

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ import layout_data.about as about
 here = os.path.abspath(os.path.dirname(__file__))
 
 
-def load_requirements(path_dir=here, comment_char='#'):
-    with open(os.path.join(path_dir, 'requirements.txt'), 'r') as file:
+def load_requirements(path_dir=here, comment_char="#"):
+    with open(os.path.join(path_dir, "requirements.txt"), "r") as file:
         lines = [line.strip() for line in file.readlines()]
     requirements = []
     for line in lines:
         # filer all comments
         if comment_char in line:
-            line = line[:line.index(comment_char)]
+            line = line[: line.index(comment_char)]
         if line:  # if requirement is not empty
             requirements.append(line)
     return requirements
@@ -26,8 +26,13 @@ setup(
     packages=find_packages(),
     install_requires=load_requirements(),
     extras_require={
-        'doc': ['sphinx', 'recommonmark', 'sphinx-rtd-theme']
+        "doc": [
+            "sphinx",
+            "recommonmark",
+            "sphinx-rtd-theme",
+            "sphinx_markdown_tables",
+        ]
     },
     include_package_data=True,
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
